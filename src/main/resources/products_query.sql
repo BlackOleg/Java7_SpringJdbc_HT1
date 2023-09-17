d1d765cd42e1cb4a2be26ade059374d976b6089e
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-09-17 23:51:46 +0400

    Ready for review

--- src/main/resources/products_query.sql
+++ src/main/resources/products_query.sql
@@ -1,4 +1,4 @@
 select product_name
 from myhometask.orders o
          join myhometask.customers c on
-    o.customer_id = c.id and LOWER(c.name) = ?;
+    o.customer_id = c.id and LOWER(c.name) = :name
